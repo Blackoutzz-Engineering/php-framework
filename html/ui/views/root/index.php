a501bb566034443cc8463ae87c0a6af07cbb875d
Mickael Nadeau <blackoutzzshoot@gmail.com>
2020-01-11 20:57:10 -0500

    Update index.php

--- html/ui/views/root/index.php
+++ html/ui/views/root/index.php
@@ -3,13 +3,3 @@
         Welcome to framework 1.0
     </div>
 </div>
-<div class="card">
-    <div class="card-body">
-        <pre>
-<?php
-var_dump($this->get_data("session"));
-var_dump($this->get_data("user"));
-?>
-        </pre>
-    </div>
-</div>
