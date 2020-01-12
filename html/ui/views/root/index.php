47ab5e2975dfffc974d676d69855bd955bb29a88
Mickael Nadeau <blackoutzzshoot@gmail.com>
2020-01-11 20:14:48 -0500

    frontend ui update
    -login page
    -register page
    -forgot password page

--- html/ui/views/root/index.php
+++ html/ui/views/root/index.php
@@ -3,3 +3,13 @@
         Welcome to framework 1.0
     </div>
 </div>
+<div class="card">
+    <div class="card-body">
+        <pre>
+<?php
+var_dump($this->get_data("session"));
+var_dump($this->get_data("user"));
+?>
+        </pre>
+    </div>
+</div>
